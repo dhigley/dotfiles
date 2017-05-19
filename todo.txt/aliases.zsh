238502abad31df6d74c6c2f91e0514fb7a106e53
Daniel Higley <daniel.higley@gmail.com>
2017-05-19 15:26:43 -0400

    Upgrade my todo list with todo.txt

---
+++ todo.txt/aliases.zsh
@@ -0,0 +1,6 @@
+# todo.sh: https://github.com/ginatrapani/todo.txt-cli
+
+alias todo='todo.sh'
+compdef todo='todo.sh'
+# alias t='todo.sh -d ~/Dropbox/Documents/todo/.todo.personal.cfg'
+# alias td='todo.sh -d ~/Dropbox/Documents/todo/.todo.work.cfg'
