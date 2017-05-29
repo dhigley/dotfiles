0eb235c378a642512060813337543c8d56965316
Daniel Higley <daniel.higley@gmail.com>
2017-05-28 23:24:21 -0400

    Update brewconfig file

--- brewconfig.sh
+++ brewconfig.sh
@@ -5,40 +5,23 @@ brew install ack
 brew install ctags
 brew install git
 brew install git-flow
+brew install heroku
+brew install hub
+brew install node
+brew install postgresql
 brew install rbenv
 brew install rbenv-default-gems
+brew install rbenv-gemset
+brew install speedtest_cli
+brew install the_silver_searcher
+brew install todo-txt
 brew install tree
 brew install vim
 brew install wget
 brew install zsh
 brew install zsh-completions
 
-# brew cask install
-brew cask install 1password
-brew cask install alarm-clock
-brew cask install appcleaner
-brew cask install bartender
-brew cask install caffeine
-brew cask install calibre
-brew cask install carbon-copy-cloner
-brew cask install dash
-brew cask install dropbox
-brew cask install duet
-brew cask install google-chrome
-brew cask install google-drive
-brew cask install google-hangouts
-brew cask install iterm2
-brew cask install java
-brew cask install microsoft-office365
-brew cask install miro-video-converter
-brew cask install steam
-brew cask install textwrangler
-brew cask install transmission
-brew cask install transmit
-brew cask install vlc
-
 # cleanup
 brew linkapps
 brew cleanup
 brew prune
-brew cask cleanup
