cdd0ef30dbac2c6e8bb112342e2e7b0e5a17123a
Daniel Higley <daniel.higley@gmail.com>
2017-06-14 18:30:53 -0400

    Updated brewconfig to include taps

--- brewconfig.sh
+++ brewconfig.sh
@@ -14,12 +14,19 @@ brew install rbenv-default-gems
 brew install rbenv-gemset
 brew install speedtest_cli
 brew install the_silver_searcher
+brew install tmux --with-utf8proc
 brew install todo-txt
 brew install tree
 brew install vim
 brew install wget
 brew install zsh
 brew install zsh-completions
+
+# install from brew taps
+brew tap caskroom/cask
+brew tap linode/cli
+brew install linode-cli
+brew tap buo/cask-upgrade
 
 # cleanup
 brew linkapps
